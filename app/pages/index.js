c8a377be07e42359420bbeadbecc1263e7b4130b
AR <74219764+AR-Student824@users.noreply.github.com>
2021-06-23 05:50:14 +0000

    lots of updates

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,100 +1,137 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect } from "react"
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Blobby</title>
-        <meta name="description" content="A friendly community on Discord and Guilded that develops applications for everyone to use." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	useEffect(() => {
+		function formatAMPM(date) {
+			var hours = date.getHours();
+			var minutes = date.getMinutes();
+			var ampm = hours >= 12 ? 'PM' : 'AM';
+			hours = hours % 12;
+			hours = hours ? hours : 12; // the hour '0' should be '12'
+			minutes = minutes < 10 ? '0' + minutes : minutes;
+			var strTime = hours + ':' + minutes + '<sup>' + date.getSeconds() + '</sup> ' + ampm;
+			return strTime;
+		}
+		function formatMonth() {
+			var month = new Date().getMonth()
+			if (month == 0) return "January"
+			if (month == 1) return "February"
+			if (month == 2) return "March"
+			if (month == 3) return "April"
+			if (month == 4) return "May"
+			if (month == 5) return "June"
+			if (month == 6) return "July"
+			if (month == 7) return "August"
+			if (month == 8) return "September"
+			if (month == 9) return "October"
+			if (month == 10) return "November"
+			if (month == 11) return "December"
 
-      <main className={styles.main}>
+		}
+		function updateTime() {
+			var date = new Date
+			document.getElementById('date').innerHTML = `🕒 ${formatMonth()} ${date.getDate()}, ${date.getFullYear()} &#8226; ${formatAMPM(date)}`
+		}
+		updateTime()
+		setInterval(() => {
+			updateTime()
+		}, 1000)
+	}, [])
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Blobby</title>
+				<meta name="description" content="A friendly community on Discord and Guilded that develops applications for everyone to use." />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-        <h1 className={styles.title}>
-          Welcome to <span className={styles.pink}>Blobby 🎉</span>
-        </h1>
+			<main className={styles.main}>
+				<h2 className={styles.date} id="date">Enable JavaScript to use this site</h2>
+				<h1 className={styles.title} id="title">
+					Welcome to <span className={styles.pink}>Blobby 🎉</span>
+				</h1>
 
-        <p className={styles.description}>
-          A friendly community that develops applications for everyone to use.
+				<p className={styles.description}>
+					A friendly community that develops applications for everyone to use.
         </p>
 
 				<div className={styles.box}>
-				<h3 style={{marginBottom:"20px"}}>
-					Blobby on Social Media
+					<h3 style={{ marginBottom: "20px" }}>
+						Blobby on Social Media
 				</h3>
-        <div className={styles.grid}>
-          <a href="https://discord.gg/new" className={styles.card}>
-            <h2>Discord &rarr;</h2>
-            <p>Join the Discord server for a friendly community and tons of emojis.</p>
-          </a>
+					<div className={styles.grid}>
+						<a href="https://discord.gg/new" className={styles.card}>
+							<h2>Discord &rarr;</h2>
+							<p>Join the Discord server for a friendly community and tons of emojis.</p>
+						</a>
 
-          <a href="https://www.guilded.gg/blob" className={styles.card}>
-            <h2>Guilded &rarr;</h2>
-            <p>Join the Guilded server to chat with the Blobby community &amp; for updates on the newest things.</p>
-          </a>
-				</div>
+						<a href="https://www.guilded.gg/blob" className={styles.card}>
+							<h2>Guilded &rarr;</h2>
+							<p>Join the Guilded server to chat with the Blobby community &amp; for updates on the newest things.</p>
+						</a>
+					</div>
 				</div>
 
 				<div className={styles.box}>
-				<h3 style={{marginBottom:"20px"}}>
-					Featured projects
+					<h3 style={{ marginBottom: "20px" }}>
+						Featured projects
 				</h3>
-				 <div className={styles.grid}>
-          <a
-            href="https://hangle.me"
-            className={styles.card}
-          >
-            <h2>Hangle &rarr;</h2>
-            <p>An open source platform for developers to talk, and share their projects on.</p>
-          </a>
+					<div className={styles.grid}>
+						<a
+							href="https://hangle.me"
+							className={styles.card}
+						>
+							<h2>Hangle &rarr;</h2>
+							<p>An open source platform for developers to talk, and share their projects on.</p>
+						</a>
 
-          <a
-            href="https://divvyr.ml"
-            className={styles.card}
-          >
-            <h2>Divvyr &rarr;</h2>
-            <p>
-              A social media application where you can discover what the world is up to.
+						<a
+							href="https://divvyr.ml"
+							className={styles.card}
+						>
+							<h2>Divvyr &rarr;</h2>
+							<p>
+								A social media application where you can discover what the world is up to.
             </p>
-          </a>
-        </div>
+						</a>
+					</div>
 				</div>
 
-					<div className={styles.box}>
-				<h3 style={{marginBottom:"20px"}}>
-					Meet the owners
+				<div className={styles.box}>
+					<h3 style={{ marginBottom: "20px" }}>
+						Meet the owners
 				</h3>
-				 <div className={styles.grid}>
-          <a
-            href="https://ar-dev.cf"
-            className={styles.card}
-          >
-            <h2>AR (founder) &rarr;</h2>
-            <p>Uh... hey there! You should join the Blobby Discord and Guilded :P</p>
-          </a>
-					<a
-            href="https://aboutdavid.me"
-            className={styles.card}
-          >
-            <h2>aboutDavid &rarr;</h2>
-            <p>aboutDavid is taking a break right now so I cannot ask him what to put here just yet</p>
-          </a>
+					<div className={styles.grid}>
+						<a
+							href="https://ar-dev.cf"
+							className={styles.card}
+						>
+							<h2>AR (founder) &rarr;</h2>
+							<p>Uh... hey there! You should join the Blobby Discord and Guilded :P</p>
+						</a>
+						<a
+							href="https://aboutdavid.me"
+							className={styles.card}
+						>
+							<h2>aboutDavid &rarr;</h2>
+							<p>aboutDavid is taking a break right now so I cannot ask him what to put here just yet</p>
+						</a>
 
-          <a
-            href="https://pyxel.ml"
-            className={styles.card}
-          >
-            <h2>Pyxel &rarr;</h2>
-            <p>
-              Pyxel will kill me if I text him so idk what to put here, unless if Pyxel notices this and tells me what to add.
+						<a
+							href="https://pyxel.ml"
+							className={styles.card}
+						>
+							<h2>Pyxel &rarr;</h2>
+							<p>
+								Pyxel will kill me if I text him so idk what to put here, unless if Pyxel notices this and tells me what to add.
             </p>
-          </a>
-        </div>
+						</a>
+					</div>
 				</div>
-      </main>
-    </div>
-  )
+			</main>
+		</div>
+	)
 }
